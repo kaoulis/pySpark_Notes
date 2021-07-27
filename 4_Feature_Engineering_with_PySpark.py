e21de07108fd552c0aeb95b4521ffd85f39aa7b7
kaoulis <kyriakoschiotis@gmail.com>
2021-07-28 01:36:23 +0300

    Passing notes for chapter 4

--- 4_Feature_Engineering_with_PySpark.py
+++ 4_Feature_Engineering_with_PySpark.py
@@ -29,15 +29,25 @@ plt.show()
 # Data Wrangling ##
 # #################
 # Dropping columns
-df = df.drop(*['NO', 'UNITNUMBER', 'CLASS', 'STREETNUMBERNUMERIC'])
-# Dropping rows (based on column values)
+df = df.drop(*['No.', 'UNITNUMBER', 'CLASS', 'STREETNUMBERNUMERIC'])
+# Dropping records (based on column values)
 df = df.where(
     ~df['ASSUMABLEMORTGAGE'].isin(['Yes w/ Qualifying', 'Yes w/No Qualifying']) | df['ASSUMABLEMORTGAGE'].isNull())
-# Dropping rows (based on column outliers - 3σ rule)
+# Dropping records (based on column outliers - 3σ rule)
 df = df.withColumn('log_SalesClosePrice', log('SALESCLOSEPRICE'))  # First transform with log because 3σ works with nearly normal data
 mean_val = df.agg({'log_SalesClosePrice': 'mean'}).collect()[0][0]
 stddev_val = df.agg({'log_SalesClosePrice': 'stddev'}).collect()[0][0]
 low_bound = mean_val - (3 * stddev_val)
 hi_bound = mean_val + (3 * stddev_val)
 df = df.filter((df['log_SalesClosePrice'] < hi_bound) & (df['log_SalesClosePrice'] > low_bound))
+# Drop any records with NULL values
+df = df.dropna()
+# Drop records if both LISTPRICE and SALESCLOSEPRICE are NULL
+df = df.dropna(how='all', subset=['LISTPRICE', 'SALESCLOSEPRICE'])
+# Drop records where at least two columns have NULL values
+df = df.dropna(thresh=2)
+# Drop duplicate records
+df.dropDuplicates()
+# Drop records based on duplicate column values
+df.dropDuplicates(['streetaddress'])
 ##
